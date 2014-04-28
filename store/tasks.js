329dfb7696177a929f1727d332aea5c2e5281c35
James Lal <james@lightsofapollo.com>
2014-04-28 12:22:45 -0700

    claim logic

--- store/tasks.js
+++ store/tasks.js
@@ -1,8 +1,9 @@
 var mem = require('memoizee');
 var slugid = require('slugid');
 var _ = require('lodash');
+var Promise = require('promise');
 
-var FIELDS = [
+var TASK_FIELDS = [
   'taskId',
   'provisionerId',
   'workerType',
@@ -17,6 +18,12 @@ var FIELDS = [
   'takenUntil'
 ];
 
+var RUN_FIELDS = [
+  'runId',
+  'workerGroup',
+  'workerId'
+];
+
 /**
 Build a record suitable for storage in the database from an external object.
 
@@ -25,7 +32,7 @@ XXX: Ideally this would be done by a "model" rather then the store in DM pattern
 @return {Object}
 */
 function incomingTask(input) {
-  var record = FIELDS.reduce(function(record, key) {
+  var record = TASK_FIELDS.reduce(function(record, key) {
     record[key] = input[key];
     return record;
   }, {});
@@ -34,15 +41,59 @@ function incomingTask(input) {
   return record;
 }
 
+function mapTask(taskId, row) {
+  var record = TASK_FIELDS.reduce(function(model, key) {
+    model[key] = row[key];
+    return model;
+  }, {});
+  record.taskId = taskId;
+  return record;
+}
+
+function mapRun(row) {
+  return RUN_FIELDS.reduce(function(model, key) {
+    model[key] = row[key];
+    return model;
+  }, {});
+}
+
 /**
 Build a record suitable for returning from the store.
 */
-function outgoingTask(input) {
-  // we can manipulate the state of the outgoing task since it is fully
-  // internal to the store and has no shared state.
-  input.taskId = slugid.encode(input.taskId);
-  return input;
+function outgoingTasks(rows) {
+  var byTaskId = {};
+
+  return rows.reduce(function(tasks, row) {
+    // extract the task by task id (only create one)
+    var taskId = slugid.encode(row.taskId);
+
+    var task = byTaskId[taskId];
+    if (!task) {
+      task = mapTask(taskId, row);
+      tasks.push(task);
+    }
+
+    if (row.runId) {
+      var runs = task.runs || (task.runs = []);
+      runs.push(mapRun(row));
+    }
+
+    return tasks;
+  }, []);
 }
+
+function decorateDecodeSlug(method) {
+  return function() {
+    var args = Array.prototype.slice.call(arguments);
+    args[0] = slugid.decode(args[0]);
+    return method.apply(this, args);
+  };
+}
+
+function first(rows) {
+  return rows[0];
+}
+
 
 module.exports = mem(function(knex) {
   return {
@@ -50,17 +101,80 @@ module.exports = mem(function(knex) {
       return knex('tasks').insert(incomingTask(task));
     },
 
-    delete: function(slug) {
-      var taskId = slugid.decode(slug);
+    delete: decorateDecodeSlug(function(taskId) {
       return knex('tasks').where('taskId', taskId).del();
+    }),
+
+    findBySlug: decorateDecodeSlug(function(taskId) {
+      return knex('tasks').
+        where('taskId', taskId).
+        limit(1).
+        then(outgoingTasks).
+        then(first);
+    }),
+
+    findBySlugWithRuns: decorateDecodeSlug(function(taskId) {
+      return knex('tasks').
+        options({ nestTables: true }).
+        join('runs', 'tasks.taskId', '=', 'runs.taskId', 'left outer').
+        where('tasks.taskId', taskId).
+        then(outgoingTasks).
+        then(first);
+    }),
+
+    claim: function(slug, takenUntil, run) {
+      if (run.runId) {
+        return this.refreshClaim(slug, takenUntil, run);
+      }
+      return this.createClaim(slug, takenUntil, run);
     },
 
-    findBySlug: function(slug) {
-      var taskId = slugid.decode(slug);
+    refreshClaim: decorateDecodeSlug(function(taskId, takenUntil, run) {
+      return knex('tasks').
+        update({
+          takenUntil: takenUntil,
+        }).
+        where('taskId', taskId).
+        andWhere('state', 'running');
+    }),
 
-      return knex('tasks').where('taskId', taskId).limit(1).then(function(list) {
-        return list[0] && outgoingTask(list[0]);
-      });
-    }
+    createClaim: decorateDecodeSlug(function(taskId, takenUntil, run) {
+      return knex.transaction(function(t) {
+        // attempt to acquire the task
+        var markRunning = knex('tasks').
+          transacting(t).
+          update({
+            retries: knex.raw('"retries" - 1'),
+            state: 'running'
+          }).
+          where('taskId', taskId).
+          andWhere('state', 'pending');
+
+        return markRunning.then(function(count) {
+          if (!count) {
+            return t.commit([]);
+          }
+
+          var runIdCount = knex('runs').
+            select(knex.raw('COUNT("runId") + 1')).
+            where('taskId', taskId).
+            toString();
+
+          return knex('runs').
+            transacting(t).
+            insert({
+              taskId: taskId,
+              workerGroup: run.workerGroup,
+              workerId: run.workerId,
+              runId: knex.raw('(' + runIdCount + ')')
+            }).
+            returning('runId').
+            then(t.commit).
+            catch(t.rollback);
+        });
+      }).
+      then(first);
+    })
+
   };
 });
--- test/store/tasks_test.js
+++ test/store/tasks_test.js
@@ -1,20 +1,12 @@
 suite('tasks', function() {
+  var slugid = require('slugid');
   var assert = require('assert');
   var Tasks;
   var testDb = require('../db');
   var knex;
 
-  setup(function() {
-    return testDb().then(function(_knex) {
-      knex = _knex;
-      Tasks = require('../../store/tasks')(knex);
-    });
-  });
-
-  var slugid = require('slugid');
-
-  test('task storage', function() {
-    var task = {
+  function taskFactory() {
+    return {
       "taskId":             slugid.v4(),
       "provisionerId":      "jonasfj-test-provid",
       "workerType":         "jonasfj-test-worker",
@@ -28,7 +20,21 @@ suite('tasks', function() {
       "deadline":           new Date("2014-03-01T03:22:36.356Z"),
       "takenUntil":         new Date("1970-01-01T00:00:00.000Z")
     };
+  }
 
+  function runFactory() {
+    return { workerId: 1, workerGroup: 'group' };
+  }
+
+  setup(function() {
+    return testDb().then(function(_knex) {
+      knex = _knex;
+      Tasks = require('../../store/tasks')(knex);
+    });
+  });
+
+  test('task storage', function() {
+    var task = taskFactory();
     return Tasks.create(task).then(function() {
       return Tasks.findBySlug(task.taskId);
     }).then(function(record) {
@@ -38,6 +44,64 @@ suite('tasks', function() {
       return Tasks.findBySlug(task.taskId);
     }).then(function(record) {
       assert.equal(record, null);
+    });
+  });
+
+  suite('#claim', function() {
+    var task;
+    setup(function() {
+      task = taskFactory();
+      return Tasks.create(task);
+    });
+
+    test('attempt to claim and fail', function() {
+      task.state = 'running';
+      return knex('tasks').
+        update({ state: 'running' }).
+        where('taskId', slugid.decode(task.taskId)).
+        then(function() {
+          return Tasks.claim(task.taskId, new Date(), {});
+        }).then(function(taskId) {
+          assert.equal(taskId, null);
+        });
+    });
+
+    test('reclaim', function() {
+      var run = runFactory();
+      var reclaimDate = new Date(2020, 1);
+      return Tasks.claim(task.taskId, new Date(), run).then(function() {
+        return Tasks.findBySlugWithRuns(task.taskId);
+      }).then(function(task) {
+        var createdRun = task.runs[0];
+        return Tasks.claim(task.taskId, reclaimDate, createdRun);
+      }).then(function(result) {
+        return Tasks.findBySlug(task.taskId);
+      }).then(function(updatedTask) {
+        assert.deepEqual(updatedTask.takenUntil, reclaimDate);
+      });
+    });
+
+    test('create first run and claim', function() {
+      var run = runFactory();
+      return Tasks.claim(task.taskId, new Date(), run).
+        then(function(runId) {
+          assert.equal(runId, 1);
+          return Tasks.findBySlugWithRuns(task.taskId);
+        }).
+        then(function(taskWithRuns) {
+          task.runs = [
+            {
+              runId: 1,
+              workerId: run.workerId,
+              workerGroup: run.workerGroup
+            }
+          ];
+
+          task.retries--;
+          task.state = 'running';
+
+          assert.deepEqual(task, taskWithRuns);
+        });
     });
   });
 });
